d46b8167e178f04537b26046af24bb45857c4d70
mKikumori <matheuskiku@gmail.com>
2026-03-15 10:49:49 -0300

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:book_app/views/landing_view.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
